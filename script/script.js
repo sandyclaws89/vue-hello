91a9495881530f43cbfce21d3657643c78cf96f9
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-10 16:05:48 +0100

    tentativi, non riesco a collegare script e html

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
 const app = new vue({
 el: '#root',
     data: {
-        message: 'Hello World',
+        message: 'ciao'
     }
 })
