f454fe50893adf74bd885d751c16d95a53409599
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-10 16:19:19 +0100

    v-model

--- script/script.js
+++ script/script.js
@@ -3,9 +3,16 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
 
-const app = new vue({
-el: '#root',
+// const app = new vue({
+// el: '#app',
+//     data: {
+//         message: ''
+//     }
+// })
+
+const app = new Vue({
+    el: '#root',
     data: {
-        message: 'ciao'
+      message: 'Hello Vue!'
     }
-})
+  })
