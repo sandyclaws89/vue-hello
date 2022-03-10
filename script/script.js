7e4e06e5eacc0136a98bfb7d32d51504d270c361
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-10 17:31:19 +0100

    test btn

--- script/script.js
+++ script/script.js
@@ -3,13 +3,7 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
 
-// const app = new vue({
-// el: '#app',
-//     data: {
-//         message: ''
-//     }
-// })
-
+/*DICHIARO LA VARIABILE APP (SPAZIO DOVE OPERA VUEJS IN HTML) E CI METTO LA FUNZIONE NEW VUE*/
 const app = new Vue({
     el: '#root',
     data: {
@@ -18,6 +12,22 @@ const app = new Vue({
       classList: {
           class1: "yellow",
           class2: "bckBlue",
-      } 
+      },
+      srcImg: "https://picsum.photos/200/200",
+    },
+    methods:{
+        btnTest1: function() {
+            this.message = '';
+            this.message = `Ho premuto il bottone e sono nel campo msg2`;
+            // this.message = '';
+            function (){
+                this.message ='';
+
+            }
+        },
+        btnTest2: function() {
+            this.message2 =`Ho premuto il bottone e sono nel campo msg2`
+        }
+
     }
-  })
+})
